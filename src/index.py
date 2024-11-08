b3de082c264d8171713377d13c2a6fcdeca7ba63
Kisute <jenhok@gmail.com>
2024-11-08 14:01:07 +0200

    koodi noudattaa kaikkia sääntöjä

--- src/index.py
+++ src/index.py
@@ -4,23 +4,18 @@ moduuli esittelee varasto luoka käyttöä
 
 from varasto import Varasto
 
-
-def main():
-    """
-    Esittelee varastoluokan käyttöä
-    """
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def print_varastot(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def print_olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def print_mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -29,6 +24,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def print_virhe_tilanteet():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -38,28 +34,48 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def print_olut_lisays(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def print_mehu_lisays(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def print_olut_otto(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def print_mehu_otto(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+
+def main():
+    """
+    Esittelee varastoluokan käyttöä
+    """
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    print_varastot(mehua, olutta)
+    print_olut_getterit(olutta)
+    print_mehu_setterit(mehua)
+    print_virhe_tilanteet()
+    print_olut_lisays(olutta)
+    print_mehu_lisays(mehua)
+    print_olut_otto(olutta)
+    print_mehu_otto(mehua)
 
 if __name__ == "__main__":
     main()
